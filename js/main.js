e6dd21aed0ad7366fc7d8d9371e0ca9160ac1a98
Tawfik Tarek <141147767+Tawfik-Tarek@users.noreply.github.com>
2023-08-03 00:50:19 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,16 +4,20 @@ const aya = document.querySelector("#aya"),
   audio = document.querySelector(".container audio");
 btn.addEventListener("click", getAya);
 async function getAya() {
-  let random = Math.floor(Math.random() * (6236 - 1) + 1);
-  const res = await fetch(`https://api.alquran.cloud/v1/ayah/${random}`);
+  let random1 = Math.floor(Math.random() * (6236 - 1) + 1);
+  let random2 = Math.floor(Math.random() * (11 - 1) + 1);
+  const res = await fetch(`https://api.alquran.cloud/v1/ayah/${random1}`);
   const res2 = await fetch(
-    `https://api.alquran.cloud/v1/ayah/${random}/ar.alafasy`
+    `https://api.quran.com/api/v4/recitations/${random2}/by_ayah/${random1}`
   );
   const data = await res.json();
   const data2 = await res2.json();
   if (res.status == 200) {
     aya.innerHTML = data.data.text;
-    audio.setAttribute("src", data2.data.audio);
+    audio.setAttribute(
+      "src",
+      "https:\/\/verses.quran.com\/" + data2.audio_files[0].url
+    );
     audio.style.display = "inline-block";
   }
 }
